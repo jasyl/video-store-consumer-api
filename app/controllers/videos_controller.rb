34a975305a8549f73de0df5d4f6e3f78b141feb3
dnsrocha <drocha@gmail.com>
2021-01-20 13:50:54 -0800

    fixed bug on add_video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -14,7 +14,7 @@ class VideosController < ApplicationController
   def add_video
     new_video = Video.new(external_id: video_params[:external_id], title: video_params[:title], overview: video_params[:overview], release_date: video_params[:release_date], image_url: video_params[:image_url])
 
-    if !Video.find_by(external_id: new_video.external_id)
+    if !Video.find_by(external_id: new_video.external_id) && new_video.external_id != nil
       if new_video.save
         render status: :ok, json: {}
       else
